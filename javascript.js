bd01e476a4a8f275f7bbf94ee168943d7ece7104
jernestmyers <inernestmusic@gmail.com>
2021-04-05 22:54:50 -0500

    copied some of the console-based RPS and deleted irrelevant parts like counting the number of draws

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
 // ROCK PAPER SCISSORS
 
+// set initial values
 const cpuOptions = [`Rock`, `Paper`, `Scissors`];
+let cpuScore = 0;
+let userScore = 0;
 
-
-// get user selection and store under "playerSelection"
+// get user and cpu selection on click event
 const userChoice = document.querySelectorAll(`.userPlay`);
     let playerSelection = ``;
     let cpuSelection = ``;
@@ -19,8 +21,32 @@ const userChoice = document.querySelectorAll(`.userPlay`);
 
 
 function playRound(playerSelection, cpuSelection) {
-    console.log(playerSelection);
-    console.log(cpuSelection);
-    
-}
+    if (playerSelection == cpuSelection) {
+        console.log(`Draw! You both threw down ${playerSelection}.`);
+    } else if   (cpuSelection == `Rock` && playerSelection == `Scissors` ||
+                cpuSelection == `Scissors` && playerSelection == `Paper` ||
+                cpuSelection == `Paper` && playerSelection == `Rock`) {
+        cpuScore += 1;
+        console.log(`You lose! ${cpuSelection} beats ${playerSelection}.`);
+    } else {
+        userScore += 1;
+        console.log(`You win! ${playerSelection} beats ${cpuSelection}.`);
+        }
+    }
 
+function playGame(playerSelection, cpuSelection) {
+    for (let round = 1; round < 6; round++) {
+        playRound(playerSelection, cpuSelection);
+        console.log(`userScore is ` + userScore);
+        console.log(`cpuScore is ` + cpuScore);
+        }
+    
+        if (userScore == 5) {
+            alert(`You win! You won ${userScore} rounds and tied ${numberDraws} rounds.`);
+        } else if (cpuScore == 5) {
+            alert(`You lose! The computer won ${cpuScore} rounds and you tied ${numberDraws} rounds.`);
+        }
+    
+        userScore = 0;
+        cpuScore = 0;
+}
